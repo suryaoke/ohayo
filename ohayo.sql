134046ddeccbd539879312a3cc235c59157c040d
suryaoke <89033805+suryaoke@users.noreply.github.com>
2023-09-02 11:22:24 +0700

    Add files via upload

---
+++ ohayo.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Sep 2023 pada 06.21
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ohayo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bakery`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.bakery: #1932 - Table 'ohayo.bakery' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.bakery: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`bakery`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `best_bakery`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.best_bakery: #1932 - Table 'ohayo.best_bakery' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.best_bakery: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`best_bakery`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `best_cake`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.best_cake: #1932 - Table 'ohayo.best_cake' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.best_cake: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`best_cake`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `best_selai`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.best_selai: #1932 - Table 'ohayo.best_selai' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.best_selai: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`best_selai`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cake`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.cake: #1932 - Table 'ohayo.cake' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.cake: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`cake`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.data: #1932 - Table 'ohayo.data' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.data: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`data`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lokasi`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.lokasi: #1932 - Table 'ohayo.lokasi' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.lokasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`lokasi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migration`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.migration: #1932 - Table 'ohayo.migration' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.migration: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`migration`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesan`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.pesan: #1932 - Table 'ohayo.pesan' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.pesan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`pesan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `selai`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.selai: #1932 - Table 'ohayo.selai' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.selai: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`selai`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+-- Kesalahan membaca struktur untuk tabel ohayo.user: #1932 - Table 'ohayo.user' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel ohayo.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ohayo`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
